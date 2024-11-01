e84f12ac076d0ef35797c679d5eda67ee6f1eaef
Trophalaxeur <trinita.andropos@gmail.com>
2024-11-01 11:38:48 +0000

    feat: add ia generated logo

--- libs/ui/header-ui/src/lib/header.tsx
+++ libs/ui/header-ui/src/lib/header.tsx
@@ -1,12 +1,11 @@
-import { Swords } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export function Header() {
   return (
     <header className="py-6">
       <Link href="/" className="flex items-center justify-center gap-2 mb-6">
-        <Swords className="h-12 w-12 text-purple-500" />
-        <span className="text-3xl font-bold">ASTON</span>
+        <Image src="/images/argon.png" alt="Argon" width="500" height="300" />
       </Link>
     </header>
   );
